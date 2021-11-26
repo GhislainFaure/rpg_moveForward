56b3c8e98584f2da571a4b855660c4f4c72ca7a8
NGambini <noe@oclock.io>
2021-11-26 10:27:06 +0100

    generation de la grille

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
 var app = {
-  init: function () {
-    console.log('init !');
-  }
+    // le div qui contient mon plateau
+    boardDiv: document.getElementById('board'),
+    player: {
+        x: 0,
+        y: 0,
+        direction: 'right'
+    },
+    targetCell: {
+        x: 5,
+        y: 3
+    },
+    drawBoard: function () {
+        // de 0 a 4...
+        for (var rowIndex = 0; rowIndex < 4; rowIndex++) {
+            // je crée un div
+            var row = document.createElement('div');
+
+            // je lui met la classe "row"
+            row.classList.add('row');
+
+            // on l'insère dans le div "boardDiv" que j'ai stocké dans une propriété de mon objet
+            app.boardDiv.appendChild(row);
+
+            for (var cellIndex = 0; cellIndex < 6; cellIndex++) {
+                // maintenant, dans cette row, on crée N cellules
+                var cell = document.createElement('div');
+
+                // je met la classe sur la cellule
+                cell.classList.add('cell');
+
+                // j'insère la cellule dans la ligne (row)
+                row.appendChild(cell);
+            }
+        }
+    },
+    init: function () {
+        console.log('init !');
+
+        app.drawBoard();
+    }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
